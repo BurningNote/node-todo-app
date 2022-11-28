9d6775d7a8aac9ffbef2db6b74df5816d7692d4d
bigstep <you@example.com>
2022-11-28 17:24:49 +0530

    added geolocation field in the todo item

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,3 +1,4 @@
+import { inject } from '@loopback/core';
 import {
   Count,
   CountSchema,
@@ -16,14 +17,17 @@ import {
   del,
   requestBody,
   response,
+  HttpErrors,
 } from '@loopback/rest';
 import {Todo} from '../models';
 import {TodoRepository} from '../repositories';
+import { Geocoder } from '../services';
 
 export class TodoController {
   constructor(
     @repository(TodoRepository)
     public todoRepository : TodoRepository,
+    @inject('services.Geocoder') protected geoService: Geocoder,
   ) {}
 
   @post('/todos')
@@ -44,6 +48,23 @@ export class TodoController {
     })
     todo: Omit<Todo, 'id'>,
   ): Promise<Todo> {
+
+    if (todo.remindAtAddress) {
+      const geo = await this.geoService.geocode(todo.remindAtAddress);
+
+      if (!geo[0]) {
+        // address not found
+        throw new HttpErrors.BadRequest(
+          `Address not found: ${todo.remindAtAddress}`,
+        );
+      }
+
+      // Encode the coordinates as "lat,lng" (Google Maps API format). See also
+      // https://stackoverflow.com/q/7309121/69868
+      // https://gis.stackexchange.com/q/7379
+      todo.remindAtGeo = `${geo[0].y},${geo[0].x}`;
+    }
+    
     return this.todoRepository.create(todo);
   }
 
